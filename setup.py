69ac45bbfbf6c718c114c470fe0ec3c2ffa815ef
Trevor Edwards <tre23@cornell.edu>
2016-01-16 16:10:18 -0500

    setup file structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Jauffre',
+    'author': 'Trevor Edwards, Jerry Jia, Elizabeth Yam',
+    'url': 'TODO',
+    'download_url': 'TODO',
+    'author_email': 'tre23@cornell.edu',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['jauffre'],
+    'scripts': [],
+    'name': 'jauffre'
+}
+
+setup(**config)
