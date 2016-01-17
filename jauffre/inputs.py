a26b3c75a1514e73dc91e861744cade669885c84
Trevor Edwards <tre23@cornell.edu>
2016-01-17 00:06:17 -0500

    parse email string

--- jauffre/inputs.py
+++ jauffre/inputs.py
@@ -6,5 +6,5 @@ def next():
     while True:
         time.sleep(5)
         res = emailio.get()
-        if res != "":
-            return res
+        if res != "" and type(res) == str:
+            return res[res.find("Content-Type: text/plain; charset=UTF-8"):]
