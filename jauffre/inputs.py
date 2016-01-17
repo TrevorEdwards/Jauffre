6cc2bcf440d550bccc6c731eea07dd0c42ee4857
Trevor Edwards <tre23@cornell.edu>
2016-01-17 09:06:23 -0500

    Disactivated voice recognition

--- jauffre/inputs.py
+++ jauffre/inputs.py
@@ -13,10 +13,10 @@ def next():
             res = emailio.get()
             if res != "" and type(res) == str:
                 return res[res.find("Content-Type: text/plain; charset=UTF-8"):]
-        rect = speech.recognize()
-        print type(rect)
-        if rect != "" and type(rect) == unicode:
-            return rect
+        # rect = speech.recognize()
+        # print type(rect)
+        # if rect != "" and type(rect) == unicode:
+        #     return rect
 
 def scan_email():
     res = emailio.get()
