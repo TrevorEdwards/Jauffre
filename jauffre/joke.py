98e051e8b7ac256c5db7b9de28a22cfb679a163d
Trevor Edwards <tre23@cornell.edu>
2016-01-16 19:22:42 -0500

    merge jxy

--- jauffre/joke.py
+++ jauffre/joke.py
@@ -1,3 +1,11 @@
+import json, urllib2
 
 #Gets a random chuck norris joke
 def get_joke_text():
+	url = "http://api.icndb.com/jokes/random"
+
+	resp = urllib2.urlopen(url = url).read()
+	decoded_resp = json.JSONDecoder().decode(resp)
+	joke = decoded_resp['value']['joke']
+
+	print joke
