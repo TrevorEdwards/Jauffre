bbc68b50576961ec26f26420027029db98bb29aa
Trevor Edwards <tre23@cornell.edu>
2016-01-17 04:39:34 -0500

    voice jauffre

--- jauffre/inputs.py
+++ jauffre/inputs.py
@@ -1,10 +1,19 @@
 #receives input from email
 import emailio
 import time
+import speech
+
 
 def next():
+    emailcount = 0
     while True:
-        time.sleep(5)
-        res = emailio.get()
-        if res != "" and type(res) == str:
-            return res[res.find("Content-Type: text/plain; charset=UTF-8"):]
+        emailcount = emailcount + 1
+        if emailcount > 5:
+            emailcount = 0
+            res = emailio.get()
+            if res != "" and type(res) == str:
+                return res[res.find("Content-Type: text/plain; charset=UTF-8"):]
+        rect = speech.recognize()
+        print type(rect)
+        if rect != "" and type(rect) == unicode:
+            return rect
